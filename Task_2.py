e7092540bf7ef302da8e3d4fbbd5f413919fb979
Даниил <@DESKTOP-FGHUH3E>
2024-09-28 09:00:36 +0300

    Добавьте файлы проекта.

---
+++ Task_2.py
@@ -0,0 +1,18 @@
+print("Перечислите 7 этапов развития человека по порядку:")
+a1 = input()
+
+a2 = input()
+
+a3 = input()
+
+a4 = input()
+
+a5 = input()
+
+a6 = input()
+
+a7 = input()
+
+print("Вы ввели:")
+print(a1,a2,a3,a4,a5,a6,a7, sep=' =>')
+
